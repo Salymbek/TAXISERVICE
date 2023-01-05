6ff8ab2f2ef3f011b2626bc7d8718f281bdc60a7
Salymbek <calumbekcaatbekov@gmail.com>
2023-01-05 14:39:42 +0600

    commit

--- src/enums/TaxiType.java
+++ src/enums/TaxiType.java
@@ -1,7 +1,33 @@
 package enums;
 
+import java.math.BigDecimal;
+
 public enum TaxiType {
-    STANDART,
-    COMFORT,
-    BUSINESS
+    STANDART(new BigDecimal(10),new BigDecimal(50)),
+    COMFORT(new BigDecimal(20),new BigDecimal(100)),
+    BUSINESS(new BigDecimal(30),new BigDecimal(150));
+
+    private BigDecimal pricePerKM;
+    private BigDecimal priceforLanding;
+
+    TaxiType(BigDecimal pricePerKM, BigDecimal priceforLanding) {
+        this.pricePerKM = pricePerKM;
+        this.priceforLanding = priceforLanding;
+    }
+
+    public BigDecimal getPricePerKM() {
+        return pricePerKM;
+    }
+
+    public void setPricePerKM(BigDecimal pricePerKM) {
+        this.pricePerKM = pricePerKM;
+    }
+
+    public BigDecimal getPriceforLanding() {
+        return priceforLanding;
+    }
+
+    public void setPriceforLanding(BigDecimal priceforLanding) {
+        this.priceforLanding = priceforLanding;
+    }
 }
--- src/model/Client.java
+++ src/model/Client.java
@@ -1,4 +1,73 @@
 package model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Client {
+    private Long id;
+    private String fullName;
+    private LocalDate DateOfBirth;
+    private String phoneNumber;
+    private BigDecimal money;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        DateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", DateOfBirth=" + DateOfBirth +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", money=" + money +
+                '}';
+    }
+
+    public Client(Long id, String fullName, LocalDate dateOfBirth, String phoneNumber, BigDecimal money) {
+        this.id = id;
+        this.fullName = fullName;
+        DateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.money = money;
+    }
+
+
 }
